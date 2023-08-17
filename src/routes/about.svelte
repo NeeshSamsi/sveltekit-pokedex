76dec133628b7daf4db5b174c329f6a11da9f1d9
Neesh <avaneeshsamsi@gmail.com>
2023-08-17 13:14:10 +0530

    Pokemon Search App - API prob changed, images don't work

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About | SvelteKit - Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool SvelteKit Demo that uses the pokemon API to build a pokedex</p>
